de198d37fbf6d67eb48b3c249d62356c502b2e62
Ashish <ashish159singh@gmail.com>
2024-03-18 23:53:27 +0530

    number challenge added

---
+++ Number Challenge/script.js
@@ -0,0 +1,11 @@
+const x = Math.floor(Math.random() * 100 + 1);
+
+const y = Math.floor(((Math.random()*100 + 1)/ 2) + 1);
+
+const sum = x + y;
+const diff = x - y;
+const pro = x * y;
+const rem = x % y;
+const div = x / y;
+
+console.log(sum,diff,pro,rem,div);
